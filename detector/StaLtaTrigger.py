36bbcb582794609e8de738547420400b8cf0e3f5
Alexey <perekhodovalexey@gmail.com>
2019-10-03 15:47:33 +0300

    first real signal probe

--- detector/StaLtaTrigger.py
+++ detector/StaLtaTrigger.py
@@ -23,16 +23,16 @@ class StaLtaTriggerCore:
         self.buf = self.lta.copy()
 
     def trigger(self, data):
+        self.buf = self.buf[-self.nlta:]
+        self.sta = self.sta[-self.nsta:]
+        self.lta = self.lta[-self.nlta:]
         for data_val in data ** 2:
             next_sta = self.sta[-1] + (data_val - self.buf[-self.nsta]) / self.nsta
             self.sta = np.append(self.sta, next_sta)
             next_lta = self.lta[-1] + (data_val - self.buf[0]) / self.nlta
             self.lta = np.append(self.lta, next_lta)
-            self.buf = np.append(self.buf, data_val)
-        ret_val = self.sta[-data.size:] / self.lta[-data.size:]
-        self.sta = self.sta[-self.nsta:]
-        self.lta = self.sta[-self.nlta:]
-        return ret_val
+            self.buf = np.append(self.buf, data_val)[1:]
+        return self.sta[-data.size:] / self.lta[-data.size:]
 
 
 class StaLtaTrigger:
@@ -89,64 +89,69 @@ def sta_lta_picker(station, channel, freqmin, freqmax, sta, lta, init_level, sto
         if events_list:
             print('events_list:' + str(events_list))
 
-st = read('D:/converter_data/example/onem.mseed')
-tr = st[0]
-sta = 2
-lta = 10
-nsta = int(tr.stats.sampling_rate * sta)
-nlta = int(tr.stats.sampling_rate * lta)
-tr_triggered = tr.copy()
-tr_triggered.stats.station = 'tri'
-data = tr.data
-slTrigger = StaLtaTriggerCore(nsta, nlta)
-# tr.data = slTrigger.trigger(data)
-data_trigger = np.empty(0, 'float32')
-for tr_chunked in tr / 10:
-    data = slTrigger.trigger(tr_chunked.data)
-    data_trigger = np.append(data_trigger, data)
-print('data_trigger size:' + str(data_trigger.size))
-tr_triggered.data = data_trigger
-tr_classic = tr.copy()
-tr_classic.trigger(type='classicstalta', sta=sta, lta=lta)
-tr_classic.stats.station = 'cla'
-(Stream() + tr + tr_triggered + tr_classic).plot(equal_scale=False)
+# st = read('D:/converter_data/example/onem.mseed')
+# tr = st[0]
+# sta = 2
+# lta = 10
+# nsta = int(tr.stats.sampling_rate * sta)
+# nlta = int(tr.stats.sampling_rate * lta)
+# tr_triggered = tr.copy()
+# tr_triggered.stats.station = 'tri'
+# data = tr.data
+# slTrigger = StaLtaTrigger(nsta, nlta)
+# # tr.data = slTrigger.trigger(data)
+# data_trigger = np.empty(0, 'float32')
+# for tr_chunked in tr / 10:
+#     data = slTrigger.trigger(tr_chunked.data)
+#     data_trigger = np.append(data_trigger, data)
+# print('data_trigger size:' + str(data_trigger.size))
+# tr_triggered.data = data_trigger
+# tr_classic = tr.copy()
+# tr_classic.trigger(type='classicstalta', sta=sta, lta=lta)
+# tr_classic.stats.station = 'cla'
+# (Stream() + tr + tr_triggered + tr_classic).plot(equal_scale=False)
 
-# def sender_test():
-#     st = read('d:/converter_data/example/onem.mseed')
-#     tr = st[0]
-#     tr.stats.station = 'ND01'
-#     tr.stats.channel = 'X'
-#     st = Stream() + tr
-#     signal_generator = SignalGenerator(st)
-#
-#     context = zmq.Context()
-#     socket = context.socket(zmq.PUB)
-#     socket.bind('tcp://*:5559')
-#
-#     pyplot.ion()
-#     figure = pyplot.figure()
-#
-#     while True:
-#         st = signal_generator.get_stream()
-#         pyplot.clf()
-#         st.plot(fig=figure)
-#         pyplot.show()
-#         sts = chunk_stream(st)
-#         for st in sts:
-#             tr = st[0]
-#             bin_header = pack_ch_header(tr.stats.station, tr.stats.channel, tr.stats.sampling_rate,
-#                                         tr.stats.starttime._ns)
-#             bin_data = bin_header + tr.data.tobytes()
-#             #print('data len:' + str(len(tr.data.tobytes())))
-#             # print('bin_data size:' + str(len(bin_data)))
-#             socket.send(bin_data)
-#             #print('bin header sent:' + str(bin_header))
-#         pyplot.pause(.5)
-#         #time.sleep(.5)
-#
-#
-# if __name__ == '__main__':
-#     p_sender = Process(target=sender_test, args=())
-#     p_receiver = Process(target=sta_lta_picker, args=('ND01', 'X', 100, 300, 1, 4, 2, 1))
-#     p_sender.start()
-#     p_receiver.start()
+def sender_test():
+    st = read('d:/converter_data/example/onem.mseed')
+    tr = st[0]
+    tr.stats.station = 'ND01'
+    tr.stats.channel = 'X'
+    st = Stream() + tr
+    signal_generator = SignalGenerator(st)
+
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind('tcp://*:5559')
+
+    pyplot.ion()
+    figure = pyplot.figure()
+
+    st_vis = Stream()
+    while True:
+        st = signal_generator.get_stream()
+        st_vis += st
+        st_vis.merge()
+        st_vis.trim(st_vis[0].stats.endtime - 10)
+        pyplot.clf()
+        st_vis.plot(fig=figure)
+        pyplot.show()
+        pyplot.pause(.01)
+        sts = chunk_stream(st)
+        for st in sts:
+            tr = st[0]
+            bin_header = pack_ch_header(tr.stats.station, tr.stats.channel, tr.stats.sampling_rate,
+                                        tr.stats.starttime._ns)
+            bin_data = bin_header + tr.data.tobytes()
+            #print('data len:' + str(len(tr.data.tobytes())))
+            # print('bin_data size:' + str(len(bin_data)))
+            socket.send(bin_data)
+            #print('bin header sent:' + str(bin_header))
+        pyplot.pause(.5)
+        #time.sleep(.5)
+
+
+if __name__ == '__main__':
+    p_sender = Process(target=sender_test, args=())
+    p_receiver = Process(target=sta_lta_picker, args=('ND01', 'X', 100, 300, 1, 4, 2, 1))
+    p_sender.start()
+    p_receiver.start()
