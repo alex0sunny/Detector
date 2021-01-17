573970efceb5ad4b4e8420a74cf80b094667f0c9
Alexey <perekhodovalexey@gmail.com>
2021-01-17 14:59:59 +0300

    Revert "Update web_server.py"
    This reverts commit cb203719749a04fe1e5a76c280a975dc87e43df3.
    
    # Conflicts:
    #	backend/web_server.py

--- trigger_main.py
+++ trigger_main.py
@@ -1,23 +1,20 @@
 import time
-from multiprocessing import Process, Pool
+from multiprocessing import Process
 from threading import Thread
 
-from backend.web_server import trigger_web
+import zmq
+
+from backend import web_server
+from backend.trigger_html_util import getTriggerParams, getSources, getActions, getRuleDic
 from detector.action.action_process import action_process, sms_process
 from detector.action.relay_actions import turn
 from detector.action.send_email import send_email
 from detector.action.send_sms import send_sms
 from detector.filter_trigger.rule import rule_picker
-from detector.filter_trigger.StaLtaTrigger import trigger_picker
 from detector.filter_trigger.rule_resender import resend
-from detector.misc.globals import Port, CustomThread, action_names_dic0
-from backend.trigger_html_util import getTriggerParams, getSources, getActions, getRuleDic
-from detector.misc.misc_util import to_action_rules, f_empty
+from detector.misc.globals import Port, action_names_dic0
+from detector.misc.misc_util import to_action_rules
 from detector.send_receive.signal_receiver import signal_receiver
-
-
-import zmq
-
 from detector.send_receive.triggers_proxy import triggers_proxy
 
 use_thread = True
@@ -45,7 +42,7 @@ if __name__ == '__main__':
     socket_backend.bind('tcp://*:' + str(Port.backend.value))
     socket_backend.setsockopt(zmq.SUBSCRIBE, b'AP')
 
-    Process(target=trigger_web).start()
+    Process(target=web_server).start()
 
     while True:
 
