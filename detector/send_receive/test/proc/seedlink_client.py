8539bffc9952ad9a9b38f791cd1c9ccaef941fe2
Alexey <perekhodovalexey@gmail.com>
2021-01-18 18:11:08 +0300

    Revert "Update web_server.py"
    This reverts commit cb203719749a04fe1e5a76c280a975dc87e43df3.
    
    # Conflicts:
    #	backend/web_server.py

--- detector/send_receive/test/proc/seedlink_client.py
+++ detector/send_receive/test/proc/seedlink_client.py
@@ -1,3 +1,6 @@
+from threading import Thread
+from time import sleep
+
 from obspy import *
 from obspy.clients.seedlink.easyseedlink import *
 from matplotlib import pyplot
@@ -6,28 +9,19 @@ from matplotlib import pyplot
 pyplot.ion()
 figure = pyplot.figure()
 st = Stream()
-check_time = UTCDateTime()
 
 
 def handle_data(trace):
-    print('Received the following trace:')
-    print(trace)
-    print()
-    global st, check_time
+    global st
     st += trace
-    cur_time = UTCDateTime()
-    if cur_time > check_time + 1:
-        check_time = cur_time
-        st.sort().merge()
-        st.trim(starttime=st[-1].stats.endtime - 60)
-        pyplot.clf()
-        st.plot(fig=figure)
-        pyplot.show()
-        pyplot.pause(.1)
+    endtime = st[-1].stats.endtime
+    if endtime > st[0].stats.starttime + 70:
+        st.trim(endtime - 60)
+    #print('trace %s appended to stream %s' % (trace, st))
 
 
 #client = create_client('rtserve.iris.washington.edu', on_data=handle_data)
-client = create_client('192.168.0.200', on_data=handle_data)
+client = create_client('192.168.0.226', on_data=handle_data)
 #client = create_client('81.26.81.45', on_data=handle_data)
 print(client.get_info('streams'))
 print()
@@ -35,7 +29,27 @@ print(client.get_info('ALL'))
 
 #client.select_stream('IU', 'ANMO', 'BH?')
 #client.select_stream('KS', 'SHAR', 'S2?')
-client.select_stream('KS', 'ND01', 'S0?')
+client.select_stream('RU', 'ND01', 'DN?')
 
-client.run()
+
+def f_receiver():
+    client.run()
+
+
+Thread(target=f_receiver).start()
+
+
+while True:
+    if not st:
+        sleep(1)
+        continue
+    st_vis = st[:]
+    st_vis.sort().merge()
+    endtime = st_vis[-1].stats.endtime
+    st_vis.trim(endtime - 60)
+    print('plot stream %s' % st)
+    pyplot.clf()
+    st_vis.plot(fig=figure)
+    pyplot.show()
+    pyplot.pause(1)
 
