ec8cb9c41579842f3ccc0597f8793a55b1e856df
Alexey <perekhodovalexey@gmail.com>
2021-02-26 17:34:51 +0300

    demo ndasrt module introduced

--- trigger_module.py
+++ trigger_module.py
@@ -1,8 +1,9 @@
-import os, sys, zmq
+import os, sys
 from com_main_module import COMMON_MAIN_MODULE_CLASS
 from time import sleep, time
 from subprocess import Popen, PIPE
 from signal import SIGTERM
+import json
 
 
 # This program is for demonstration purposes only!
@@ -19,24 +20,41 @@ class MAIN_MODULE_CLASS(COMMON_MAIN_MODULE_CLASS):
             'default_config': {'trigger_dir': '/var/lib/cloud9/trigger'}
         }
 
-        web_ui_dir = os.path.join(os.path.dirname(__file__), "web_ui")
+        web_ui_dir = os.path.join(os.path.dirname(__file__), "backend")
         # self._print('Initializing trigger module...')
         super().__init__(standalone, config_params, logger_config, web_ui_dir=web_ui_dir)
         config = self.get_config()
         # self._print('config:\n' + str(config) + '\n')
         sys.path.append(config['trigger_dir'])
 
+    def custom_web_ui_request(self, in_data):
+        path = in_data['path']  # .split('?')
+        for ext in ['jpg', 'png', 'ico', 'gif']:
+            if path.endswith('.' + ext):
+                f = open(self.web_ui_dir + os.sep + path, 'rb')
+                data = f.read()
+                f.close()
+                return {'binary_content': data, 'code': 200,
+                        'c_type': 'image/' + ext}
+        if in_data['type'] == 'post' and path == 'trigger':
+            content = in_data['binary_content']
+            dic = json.loads(content.decode())
+            # self._print(f'dic:{dic}')
+            triggers = dic['triggers']
+            triggers = {int(k): 0 if v else 1 for k, v in triggers.items()}
+            content = json.dumps({'triggers': triggers}).encode()
+            return {'binary_content': content, 'code': 200,
+                    'c_type': 'application/json'}
+
     def main(self):
+        workdir = os.path.dirname(__file__)
         config = self.get_config()
         # print('config:\n' + str(config) + '\n')
         # if not os.path.exists(os.path.dirname(config['file_path'])): os.mkdir(os.path.dirname(config['file_path']))
         # self.errors.append('my error')
 
-        p = Popen(['python3', config['trigger_dir'] + '/trigger_main.py'],
-                  preexec_fn=os.setsid)
         # p = Popen(['python3', '/var/lib/cloud9/trigger/trigger_main.py'],
         #             stdout=PIPE, shell=True, preexec_fn=os.setsid)
-
         if self.config.error:
             self.set_config(config)
             self.config.error = None
@@ -44,39 +62,13 @@ class MAIN_MODULE_CLASS(COMMON_MAIN_MODULE_CLASS):
             # set message
             self.message = 'Starting trigger module...'
             # sleep(1)
-            context = zmq.Context()
-            socket_sub = context.socket(zmq.SUB)
-            from detector.misc.globals import Port, Subscription
-            socket_sub.connect('tcp://localhost:' + str(Port.proxy.value))
-            socket_sub.setsockopt(zmq.SUBSCRIBE, Subscription.signal.value)
 
             # read new packets in loop, abort if connection fails or shutdown event is set
             while not self.shutdown_event.is_set():
-                if socket_sub.poll(3000) and self.errors:
-                    self.errors = []
-                    self.message = 'running'
-                    # self._print('data received')
-                    try:
-                        # self._print('flush socket')
-                        while True:
-                            socket_sub.recv(zmq.NOBLOCK)
-                    except zmq.ZMQError:
-                        # self._print('socket flushed')
-                        pass
-                    continue
-                elif not self.errors:
-                    self.errors.append('connection error')
-                    self.message = self.errors[-1]
-                    # self._print('no data')
-
-                # self._print(f'executing module time:{time()} errors:{self.get_errors_list()}')
-                # self.errors.append('my error')
+                self.message = 'running'
                 if self.errors:
                     self.message = self.get_errors_list()[-1]
-                # sleep(1)
-
-        # p.send_signal(SIGTERM)
-        os.killpg(os.getpgid(p.pid), SIGTERM)
+                sleep(1)
 
         self.module_alive = False
         # self.set_config(self.get_config())
