11e27eace697fba1d2c279ed451a04ce398aee4f
Alexey <perekhodovalexey@gmail.com>
2021-03-02 10:51:11 +0300

    malfunctioning trigger_module.py

--- trigger_module.py
+++ trigger_module.py
@@ -178,96 +178,130 @@ class MAIN_MODULE_CLASS(COMMON_MAIN_MODULE_CLASS):
             self.set_config(config)
             self.config.error = None
 
-        self.message = 'Starting trigger module...'
+        self.message = 'starting...'
 
         context = zmq.Context()
+
         socket_backend = context.socket(zmq.SUB)
         socket_backend.bind('tcp://*:' + str(Port.backend.value))
         socket_backend.setsockopt(zmq.SUBSCRIBE, b'AP')
 
-        paramsList = getTriggerParams()
-        trigger_dic = {params['ind']: params['name'] for params in paramsList}
+        socket_check = context.socket(zmq.SUB)
+        socket_check.connect('tcp://localhost:' + str(Port.proxy.value))
+        socket_check.setsockopt(zmq.SUBSCRIBE, Subscription.signal.value)
 
-        kwargs_list = []
+        starting = True
 
-        action_params = getActions()
-        action_names_dic = {}
-        action_names_dic.update(action_names_dic0)
-        sms_dic0 = action_params.get('sms', {})
-        sms_dic = {sms_id: sms_dic0[sms_id]['name'] for sms_id in sms_dic0}
-        action_names_dic.update(sms_dic)
-        rule_dic = getRuleDic()
-        rule_actions = {rule: rule_dic[rule]['actions'] for rule in rule_dic}
-        action_rules = to_action_rules(rule_actions)
-        send_signal_params = action_params['seedlk']
-        pem = send_signal_params['pem']
-        pet = send_signal_params['pet']
-        rules = []
-        if 3 in action_rules:
-            rules = action_rules[3]
-        kwargs_list += [{'target': resend, 'kwargs': {'conn_str': 'tcp://*:' + str(Port.signal_resend.value),
-                                                      'rules': rules, 'pem': pem, 'pet': pet}},
-                        {'target': triggers_proxy, 'kwargs': {}}]
-        for action_type, send_func in {'email': send_email, 'sms': send_sms}.items():
-            if action_type in action_params:
-                send_params_dic = action_params[action_type]
-                for action_id in send_params_dic:
+        while not self.shutdown_event.is_set():
+            if starting:
+                logger.debug('load module params from html files')
+                paramsList = getTriggerParams()
+                trigger_dic = {params['ind']: params['name'] for params in paramsList}
+
+                kwargs_list = []
+
+                action_params = getActions()
+                action_names_dic = {}
+                action_names_dic.update(action_names_dic0)
+                sms_dic0 = action_params.get('sms', {})
+                sms_dic = {sms_id: sms_dic0[sms_id]['name'] for sms_id in sms_dic0}
+                action_names_dic.update(sms_dic)
+                rule_dic = getRuleDic()
+                rule_actions = {rule: rule_dic[rule]['actions'] for rule in rule_dic}
+                action_rules = to_action_rules(rule_actions)
+                send_signal_params = action_params['seedlk']
+                pem = send_signal_params['pem']
+                pet = send_signal_params['pet']
+                rules = []
+                if 3 in action_rules:
+                    rules = action_rules[3]
+                kwargs_list += [{'target': resend, 'kwargs': {'conn_str': 'tcp://*:' + str(Port.signal_resend.value),
+                                                              'rules': rules, 'pem': pem, 'pet': pet}},
+                                {'target': triggers_proxy, 'kwargs': {}}]
+                for action_type, send_func in {'email': send_email, 'sms': send_sms}.items():
+                    if action_type in action_params:
+                        send_params_dic = action_params[action_type]
+                        for action_id in send_params_dic:
+                            rules = []
+                            if action_id in action_rules:
+                                rules = action_rules[action_id]
+                            send_func_params = send_params_dic[action_id]
+                            del send_func_params['name']
+                            detrigger = send_func_params.pop('detrigger')
+                            kwargs = {'action_id': action_id, 'rules': rules, 'send_func': send_func,
+                                      'args': send_func_params, 'detrigger': detrigger}
+                            kwargs_list.append({'target': sms_process, 'kwargs': kwargs})
+                for action_id, relay_k in zip([1, 2], ['relayA', 'relayB']):
                     rules = []
                     if action_id in action_rules:
                         rules = action_rules[action_id]
-                    send_func_params = send_params_dic[action_id]
-                    del send_func_params['name']
-                    detrigger = send_func_params.pop('detrigger')
-                    kwargs = {'action_id': action_id, 'rules': rules, 'send_func': send_func,
-                              'args': send_func_params, 'detrigger': detrigger}
-                    kwargs_list.append({'target': sms_process, 'kwargs': kwargs})
-        for action_id, relay_k in zip([1, 2], ['relayA', 'relayB']):
-            rules = []
-            if action_id in action_rules:
-                rules = action_rules[action_id]
-            # print('pet:' + str(action_params['relay'][relay_k]['pet']))
-            kwargs_list.append({'target': action_process,
-                                'kwargs': {'action_id': action_id, 'rules': rules, 'send_func': turn,
-                                           'args': {'inverse': action_params['relay'][relay_k]['inverse']},
-                                           'infinite': action_params['relay'][relay_k]['infinite'],
-                                           'pet': action_params['relay'][relay_k]['pet']}})
-        triggers_params = {}
-        for params in paramsList:
-            # params.update({'init_level': 2, 'stop_level': 1})
-            trigger_params = params.copy()
-            del trigger_params['name']
-            trigger_params['trigger_id'] = trigger_params.pop('ind')
-            station = trigger_params['station']
-            channel = trigger_params['channel']
-            if station not in triggers_params:
-                triggers_params[station] = {channel: []}
-            elif channel not in triggers_params[station]:
-                triggers_params[station][channel] = []
-            del trigger_params['station']
-            del trigger_params['channel']
-            triggers_params[station][channel].append(trigger_params)
-            # kwargs_list.append({'target': trigger_picker, 'kwargs': trigger_params})
-        for station, conn_data in getSources().items():
-            kwargs = {'target': signal_receiver,
-                      'kwargs': {'conn_str': 'tcp://' + conn_data['host'] + ':' + str(conn_data['port']),
-                                 'station_bin': station.encode(),
-                                 'triggers_params': triggers_params[station]}}
-            kwargs_list.append(kwargs)
-        for rule_id in sorted(rule_dic.keys()):
-            formula_list = rule_dic[rule_id]['formula']
-            kwargs_list.append({'target': rule_picker, 'kwargs': {'rule_id': rule_id, 'formula_list': formula_list}})
+                    # print('pet:' + str(action_params['relay'][relay_k]['pet']))
+                    kwargs_list.append({'target': action_process,
+                                        'kwargs': {'action_id': action_id, 'rules': rules, 'send_func': turn,
+                                                   'args': {'inverse': action_params['relay'][relay_k]['inverse']},
+                                                   'infinite': action_params['relay'][relay_k]['infinite'],
+                                                   'pet': action_params['relay'][relay_k]['pet']}})
+                triggers_params = {}
+                for params in paramsList:
+                    # params.update({'init_level': 2, 'stop_level': 1})
+                    trigger_params = params.copy()
+                    del trigger_params['name']
+                    trigger_params['trigger_id'] = trigger_params.pop('ind')
+                    station = trigger_params['station']
+                    channel = trigger_params['channel']
+                    if station not in triggers_params:
+                        triggers_params[station] = {channel: []}
+                    elif channel not in triggers_params[station]:
+                        triggers_params[station][channel] = []
+                    del trigger_params['station']
+                    del trigger_params['channel']
+                    triggers_params[station][channel].append(trigger_params)
+                    # kwargs_list.append({'target': trigger_picker, 'kwargs': trigger_params})
+                for station, conn_data in getSources().items():
+                    kwargs = {'target': signal_receiver,
+                              'kwargs': {'conn_str': 'tcp://' + conn_data['host'] + ':' + str(conn_data['port']),
+                                         'station_bin': station.encode(),
+                                         'triggers_params': triggers_params[station]}}
+                    kwargs_list.append(kwargs)
+                for rule_id in sorted(rule_dic.keys()):
+                    formula_list = rule_dic[rule_id]['formula']
+                    kwargs_list.append(
+                        {'target': rule_picker, 'kwargs': {'rule_id': rule_id, 'formula_list': formula_list}})
+                logger.debug('parameters loaded')
 
-        threads_proc = Process(target=fps, args=(kwargs_list,))
-        threads_proc.start()
+                threads_proc = Process(target=fps, args=(kwargs_list,))
+                logger.debug('start threads_proc')
+                threads_proc.start()
+                logger.debug('process started')
 
-        while not self.shutdown_event.is_set():
-            sleep(1)
-            self.message = 'running'
+            if socket_backend.poll(0):
+                logger.debug('restart command received')
+                socket_backend.recv()
+                starting = True
+                sleep(1)
+                threads_proc.terminate()
+                self.message = 'restarting...'
+            else:
+                # logger.debug('poll signal')
+                if socket_check.poll(20000 if starting else 3000):
+                    if self.errors:
+                        self.errors = []
+                else:
+                    logger.warning('signal poll timeout')
+                    if not self.errors:
+                        self.errors.append('connection error')
+                        self.message = self.errors[-1]
+                if starting:
+                    self.message = 'running'
+                starting = False
 
         threads_proc.terminate()
-        self.message = 'stopped'
+        context.close()
+        logger.debug('trigger module terminated')
+        self.message = 'stopped'  # unnecessary?
 
         self.module_alive = False
         # self.set_config(self.get_config())
         self._print('trigger module thread exited')
+
 
