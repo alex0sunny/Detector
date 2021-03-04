2229da3e197fbe84815821d0bb2a23066874bcf7
Alexey <perekhodovalexey@gmail.com>
2021-03-04 14:32:01 +0300

    conditionally functioning with parameters application, no proper status indication

--- trigger_module.py
+++ trigger_module.py
@@ -3,6 +3,7 @@ from com_main_module import COMMON_MAIN_MODULE_CLASS
 from time import sleep, time
 from signal import SIGTERM
 from obspy import UTCDateTime
+from subprocess import Popen, PIPE
 
 sys.path.append(os.path.dirname(__file__))
 
@@ -10,7 +11,8 @@ from backend.trigger_html_util import save_pprint_trig, getTriggerParams, \
     save_triggers, update_sockets, post_triggers, \
     save_sources, save_rules, update_rules, apply_sockets_rule, save_actions, \
     update_triggers_sockets, getActions, getRuleDic, getSources
-from detector.misc.globals import Port, Subscription, action_names_dic0, logger
+from detector.misc.globals import Port, Subscription, action_names_dic0, \
+    logger, CustomThread
 
 from threading import Thread
 from multiprocessing import Process
@@ -29,14 +31,14 @@ from detector.send_receive.triggers_proxy import triggers_proxy
 def fps(kwargs_list):
     ps = []
     for kwargs in kwargs_list:
-        p = Thread(**kwargs)
+        p = CustomThread(**kwargs)
         p.start()
         ps.append(p)
-    for p in ps:
-        p.join()
+    return ps
 
 
 context = zmq.Context()
+
 socket_backend = context.socket(zmq.PUB)
 socket_backend.connect('tcp://localhost:' + str(Port.backend.value))
 
@@ -93,7 +95,7 @@ class MAIN_MODULE_CLASS(COMMON_MAIN_MODULE_CLASS):
         super().__init__(standalone, config_params, logger_config, web_ui_dir=web_ui_dir)
         config = self.get_config()
         # self._print('config:\n' + str(config) + '\n')
-        sys.path.append(config['trigger_dir'])
+        self.restarting = False
 
     def custom_web_ui_request(self, in_data):
         path = in_data['path']  # .split('?')
@@ -148,6 +150,7 @@ class MAIN_MODULE_CLASS(COMMON_MAIN_MODULE_CLASS):
                 sockets_rule = get_rule_sockets(session_id)
                 apply_sockets_rule(conn_str_sub, context, sockets_rule)
                 socket_backend.send(b'AP')
+                self.restarting = True
             if path == 'save':
                 session_id = request_dic['sessionId']
                 html = request_dic['html']
@@ -155,12 +158,15 @@ class MAIN_MODULE_CLASS(COMMON_MAIN_MODULE_CLASS):
                 sockets_trigger = get_sockets_data(session_id)
                 update_triggers_sockets(conn_str_sub, context, sockets_trigger)
                 socket_backend.send(b'AP')
+                self.restarting = True
             if path == 'saveSources':
                 save_sources(content.decode())
                 socket_backend.send(b'AP')
+                self.restarting = True
             if path == 'applyActions':
                 save_actions(content.decode())
                 socket_backend.send(b'AP')
+                self.restarting = True
             if path == 'testActions':
                 ids = request_dic['ids']
                 for action_id in ids:
@@ -179,108 +185,51 @@ class MAIN_MODULE_CLASS(COMMON_MAIN_MODULE_CLASS):
     def main(self):
         workdir = os.path.dirname(__file__)
         config = self.get_config()
+        p = Popen(['python3', workdir + '/trigger_main.py'],
+                  preexec_fn=os.setsid)
+        # p = Popen(['python3', '/var/lib/cloud9/trigger/trigger_main.py'],
+        #             stdout=PIPE, shell=True, preexec_fn=os.setsid)
+
         if self.config.error:
             self.set_config(config)
             self.config.error = None
-
-        self.message = 'Starting trigger module...'
-
-        context = zmq.Context()
-        socket_backend = context.socket(zmq.SUB)
-        socket_backend.bind('tcp://*:' + str(Port.backend.value))
-        socket_backend.setsockopt(zmq.SUBSCRIBE, b'AP')
-
+        self.message = 'Starting...'
+        check_time = UTCDateTime() + 60
         while not self.shutdown_event.is_set():
-            sleep(1)
-            paramsList = getTriggerParams()
-            trigger_dic = {params['ind']: params['name'] for params in paramsList}
+            # sleep(1)
+            context = zmq.Context()
+            socket_sub = context.socket(zmq.SUB)
+            from detector.misc.globals import Port, Subscription
+            socket_sub.connect('tcp://localhost:' + str(Port.proxy.value))
+            socket_sub.setsockopt(zmq.SUBSCRIBE, Subscription.signal.value)
 
-            kwargs_list = []
-
-            action_params = getActions()
-            action_names_dic = {}
-            action_names_dic.update(action_names_dic0)
-            sms_dic0 = action_params.get('sms', {})
-            sms_dic = {sms_id: sms_dic0[sms_id]['name'] for sms_id in sms_dic0}
-            action_names_dic.update(sms_dic)
-            rule_dic = getRuleDic()
-            rule_actions = {rule: rule_dic[rule]['actions'] for rule in rule_dic}
-            action_rules = to_action_rules(rule_actions)
-            send_signal_params = action_params['seedlk']
-            pem = send_signal_params['pem']
-            pet = send_signal_params['pet']
-            rules = []
-            if 3 in action_rules:
-                rules = action_rules[3]
-            kwargs_list += [{'target': resend, 'kwargs': {'conn_str': 'tcp://*:' + str(Port.signal_resend.value),
-                                                          'rules': rules, 'pem': pem, 'pet': pet}},
-                            {'target': triggers_proxy, 'kwargs': {}}]
-            for action_type, send_func in {'email': send_email, 'sms': send_sms}.items():
-                if action_type in action_params:
-                    send_params_dic = action_params[action_type]
-                    for action_id in send_params_dic:
-                        rules = []
-                        if action_id in action_rules:
-                            rules = action_rules[action_id]
-                        send_func_params = send_params_dic[action_id]
-                        del send_func_params['name']
-                        detrigger = send_func_params.pop('detrigger')
-                        kwargs = {'action_id': action_id, 'rules': rules, 'send_func': send_func,
-                                  'args': send_func_params, 'detrigger': detrigger}
-                        kwargs_list.append({'target': sms_process, 'kwargs': kwargs})
-            for action_id, relay_k in zip([1, 2], ['relayA', 'relayB']):
-                rules = []
-                if action_id in action_rules:
-                    rules = action_rules[action_id]
-                # print('pet:' + str(action_params['relay'][relay_k]['pet']))
-                kwargs_list.append({'target': action_process,
-                                    'kwargs': {'action_id': action_id, 'rules': rules, 'send_func': turn,
-                                               'args': {'inverse': action_params['relay'][relay_k]['inverse']},
-                                               'infinite': action_params['relay'][relay_k]['infinite'],
-                                               'pet': action_params['relay'][relay_k]['pet']}})
-            triggers_params = {}
-            for params in paramsList:
-                # params.update({'init_level': 2, 'stop_level': 1})
-                trigger_params = params.copy()
-                del trigger_params['name']
-                trigger_params['trigger_id'] = trigger_params.pop('ind')
-                station = trigger_params['station']
-                channel = trigger_params['channel']
-                if station not in triggers_params:
-                    triggers_params[station] = {channel: []}
-                elif channel not in triggers_params[station]:
-                    triggers_params[station][channel] = []
-                del trigger_params['station']
-                del trigger_params['channel']
-                triggers_params[station][channel].append(trigger_params)
-                # kwargs_list.append({'target': trigger_picker, 'kwargs': trigger_params})
-            for station, conn_data in getSources().items():
-                kwargs = {'target': signal_receiver,
-                          'kwargs': {'conn_str': 'tcp://' + conn_data['host'] + ':' + str(conn_data['port']),
-                                     'station_bin': station.encode(),
-                                     'triggers_params': triggers_params[station]}}
-                kwargs_list.append(kwargs)
-            for rule_id in sorted(rule_dic.keys()):
-                formula_list = rule_dic[rule_id]['formula']
-                kwargs_list.append(
-                    {'target': rule_picker, 'kwargs': {'rule_id': rule_id, 'formula_list': formula_list}})
-
-            threads_proc = Process(target=fps, args=(kwargs_list,))
-            threads_proc.start()
-
+            # read new packets in loop, abort if connection fails or shutdown event is set
             while not self.shutdown_event.is_set():
-                poll_result = socket_backend.poll(1000)
-                if poll_result:
-                    socket_backend.recv()
-                    break
-                self.message = 'running'
+                if self.restarting:
+                    self.errors = []
+                    self.message = 'Restarting...'
+                    check_time = UTCDateTime() + 60
+                    self.restarting = False
+                if socket_sub.poll(3000):
+                    self.errors = []
+                    self.message = 'Running'
+                    # self._print('data received')
+                    try:
+                        # self._print('flush socket')
+                        while True:
+                            socket_sub.recv(zmq.NOBLOCK)
+                    except zmq.ZMQError:
+                        # self._print('socket flushed')
+                        pass
+                    continue
+                elif UTCDateTime() < check_time:
+                    if self.message != 'Starting...':
+                        self.message = 'Restarting...'
+                else:
+                    self.errors = ['No stations online']
+                    self.message = self.errors[-1]
 
-            threads_proc.terminate()
-            sleep(1)
-
-        self.message = 'stopped'
-
+        os.killpg(os.getpgid(p.pid), SIGTERM)
         self.module_alive = False
-        # self.set_config(self.get_config())
-        self._print('trigger module thread exited')
+        self._print('Main thread exited')
 
